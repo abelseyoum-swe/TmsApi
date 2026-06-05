eb5579f0def4b03538ab181bdf2524239bbc3d37
Abel Seyoum <abelseyoum.swe@gmail.com>
2026-06-05 21:35:17 +0300

    feat(module-4/session-1): The Blind Server (Middleware Ordering)

--- Program.cs
+++ Program.cs
@@ -1,17 +1,53 @@
+// ==== Exercise 1 - The Blind Server (Middleware Ordering) ====
+
+// // == Predict before you change code ==
+// // Starter pipeline (do nos assume this order is correct)
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+
+// app.UseRouting();
+
+// app.MapGet("/api/assessment/results", () => Results.Ok(new
+// {
+//     courseCode = "CS-101",
+//     studentId = "S-001",
+//     letterGrade = "A"
+// }));
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.Run();
+
+// == Task: Secure the pipeline ==
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Services: add authentication / authorization service
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = "https://your-auth-server";
+    options.Audience = "tms-api";
+}); // Sample Authentication implementation for test purpose
 
-builder.Services.AddControllers();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// TODO 1: Register routing in the pipeline where it belongs for your app.
+app.UseRouting();
 
-app.UseHttpsRedirection();
-
+// TODO 2: Register authentication and authorization in the pipeline where your template and facilitator expect them for a protected minimal API route.
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+// TODO 3: Map GET /api/assessments/results with the same response body as the starter, but require authorization for that route.
+app.MapGet("/api/assessments/results", () => Results.Ok(new
+{
+    courseCode = "CS-101",
+    studentId = "S-001",
+    letterGrade = "A"
+}))
+.RequireAuthorization();
 
 app.Run();
